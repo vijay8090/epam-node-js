7a3b6999ab5068039f04fb820720df1ad482bc28
vijay8090 <vijayakumar.anbu1@gmail.com>
2021-11-12 10:29:35 +0530

    2 files modified

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 // run `node index.js` in the terminal
+import departments from './data/departments';
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+console.log(`Hello 123 Node.js v${process.versions.node}!`);
+
+console.log(`departments` + departments);
