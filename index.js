816535c1d722041a490478a46bc9c2bd87522243
vijay8090 <vijayakumar.anbu1@gmail.com>
2021-11-12 10:42:22 +0530

    1 file modified

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const departments = require('./data/departments');
 
 console.log(`Hello 123 Node.js v${process.versions.node}!`);
 
-console.log(`departments` + departments);
+console.log(`departments` + JSON.stringify(departments,null,2));
